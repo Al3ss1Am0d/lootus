9c22520eb9e1086b2ece46fb46c7315fdfb1f8d2
Alessio Kun <131905914+Al3ss1Am0d@users.noreply.github.com>
2024-02-19 16:25:05 +0200

    ok

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const openBtn = document.querySelector("#openBtn");
 card = document.querySelector(".card");
 closeBtn = document.querySelector(".closeBtn");
+loginLink = document.querySelector(".loginLink")
+registerLink = document.querySelector(".registerLink");
+containerform = document.querySelector(".containerform")
+
+
 
 openBtn.addEventListener("click", ()=>{
     card.classList.add('show'); // feche o parêntese aqui
@@ -8,4 +13,14 @@ openBtn.addEventListener("click", ()=>{
 
 closeBtn.addEventListener("click", ()=>{
     card.classList.remove('show');
+})
+
+registerLink.addEventListener('click',(e)=>{
+    e.preventDefault();
+    containerform.classList.add('active');
+})
+
+loginLink.addEventListener('click',(e)=>{
+    e.preventDefault();
+    containerform.classList.remove('active');
 })
