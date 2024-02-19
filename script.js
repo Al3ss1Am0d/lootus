31c8e5e03365aeb009e8f0a70dcb30f97dff13a4
Alessio Kun <131905914+Al3ss1Am0d@users.noreply.github.com>
2024-02-19 16:01:25 +0200

    ok

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const openBtn = document.querySelector("#openBtn");
+    card = document.querySelector(".card);
+    closeBtn = document.querySelector(".closeBtn");
 
+
+
+openBtn.addEventListener("click",()=>(
+    card.classList.add('show');
+))
+
+closeBtn.addEventListener("click",()=>(
+    card.classList.remove('show');
+))
